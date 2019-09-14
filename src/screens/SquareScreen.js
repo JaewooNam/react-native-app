acd62703db14084104442b68e6d9f6b8b8c9bef1
JaewooNam <namssabu12@gmail.com>
2019-09-14 21:54:36 +0900

    reducer convention

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,14 +5,13 @@ import ColorCounter from '../components/ColorCounter';
 const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
-
-  switch (action.colorToChange) {
-    case 'red':
+  switch (aciton.type) {
+    case 'change_red':
       // never going to do like state.red = state.red - 15..
       return {...state, red: state.red + action.amount }
-    case 'green':
+    case 'change_green':
       return {...state, green: state.green + action.amount }
-    case 'blue':
+    case 'change_blue':
       return {...state, blue: state.blue + action.amount }
     default:
       return state;
@@ -26,18 +25,18 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type: 'change_red', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type: 'change_red', payload: -1 * COLOR_INCREMENT})}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type: 'change_blue', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT})}
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type: 'change_green', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type: 'change_green', payload: -1 * COLOR_INCREMENT})}
         color="Green"
       />
       <View style={{
